881c3696adb1fe632d0a69299edcc55d9e8570ae
samkellerhals <kellerhalssamuel@gmail.com>
2020-03-15 18:13:16 +0100

    add frontend tests for homepage

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,29 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+// Homepage tests
+
 test('Render application title', () => {
   const { getByText } = render(<App />);
-  const title = getByText('MedAssistant');
-  expect(title).toBeInTheDocument();
+  const element = getByText('MedAssistant');
+  expect(element).toBeInTheDocument();
+});
+
+test('Render login button', () => {
+  const { getByText } = render(<App />);
+  const element = getByText('Sign in');
+  expect(element).toBeInTheDocument();
+});
+
+test('Render signup button', () => {
+  const { getByText } = render(<App />);
+  const element = getByText('Sign up');
+  expect(element).toBeInTheDocument();
+});
+
+
+test('Render about page', () => {
+  const { getByText } = render(<App />);
+  const element = getByText('About');
+  expect(element).toBeInTheDocument();
 });
